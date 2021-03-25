77f8478bcca48e6041fb5da618997c9e34548ba0
nfour <novus.nfour@gmail.com>
2021-03-25 17:17:44 +1000

    move files around

--- react-app-2/src/index.tsx
+++ react-app-2/src/index.tsx
@@ -1,4 +1,21 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Routes } from "./routing";
+import { NavBar } from "./navBar";
+import { RootStore, RootStoreReactContext } from "./store";
 import "./global.css";
-import "./mobx";
+
+function Root() {
+  const [store] = React.useState(() => new RootStore());
+
+  return (
+    <RootStoreReactContext.Provider value={store}>
+      <NavBar />
+      <Routes />
+    </RootStoreReactContext.Provider>
+  );
+}
+
+render(<Root />, document.getElementById("__root"));
 
 if (import.meta.hot) import.meta.hot.accept();
