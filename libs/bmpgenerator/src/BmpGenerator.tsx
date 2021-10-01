6b9c98ed1d6d1011f64658c21dac0a72bbbc2d0d
nfour <novus.nfour@gmail.com>
2021-10-01 13:09:01 +1000

    Fixes

--- libs/bmpgenerator/src/BmpGenerator.tsx
+++ libs/bmpgenerator/src/BmpGenerator.tsx
@@ -14,6 +14,8 @@ import {
 import styled from '@emotion/styled';
 import { BooleanModel, StateModel } from '@dhi/arsenal.models';
 import './styles.css';
+import { PropsOf } from '@emotion/react';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 DocumentEditorContainerComponent.Inject(
   Toolbar,
@@ -39,126 +41,138 @@ export interface PreviewBody extends ReplacementProps {
 
 export { DocumentEditorContainer };
 
+export class BmpGeneratorState {
+  serviceUrl: StateModel<string>;
+  constructor({ serviceUrl }: { serviceUrl: string }) {
+    this.serviceUrl = new StateModel(serviceUrl);
+
+    makeAutoObservable(this);
+  }
+
+  documentIsReady = new BooleanModel(false);
+  documentLastChanged = new StateModel(Date.now());
+  documentEditor = new StateModel<DocumentEditorContainer | undefined>(
+    undefined,
+  );
+
+  renderedDocumentEditor = new StateModel<DocumentEditorContainer | undefined>(
+    undefined,
+  );
+
+  isPreviewOpen = new BooleanModel(false);
+
+  replacements = new StateModel<ReplacementProps | undefined>(undefined);
+
+  get editor() {
+    return this.documentEditor.value;
+  }
+
+  get renderingEditor() {
+    return this.renderedDocumentEditor.value;
+  }
+
+  initEditor = (editor?: DocumentEditorContainer) => {
+    if (!editor) return;
+
+    editor.resize();
+  };
+
+  save = () => {
+    const data = this.editor?.documentEditor.serialize();
+
+    console.log({ data });
+
+    this.editor?.documentEditor.save('file.docx', 'Docx');
+  };
+
+  loadDocumentRender = async () => {
+    const blob = await this.editor!.documentEditor.saveAsBlob('Docx')!;
+    const file = new FileReader();
+
+    file.addEventListener('loadend', async (e) => {
+      const base64String = file.result;
+
+      const res = await fetch(
+        `${this.serviceUrl.value}/PreviewParameterReplacement`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+          },
+          body: JSON.stringify({
+            fileName: this.editor?.documentEditor.documentName,
+            documentContent: base64String,
+            textReplacements: [
+              ...(this.replacements.value?.textReplacements ?? []),
+            ],
+            imageReplacements: [
+              ...(this.replacements.value?.imageReplacements ?? []),
+            ],
+            tableReplacements: [
+              ...(this.replacements.value?.tableReplacements ?? []),
+            ],
+          } as PreviewBody),
+        },
+      );
+
+      const text = await res.text();
+
+      this.loadRender(text);
+    });
+
+    file.readAsDataURL(blob);
+  };
+
+  loadRender = (text: string) => {
+    console.log({
+      documentText: text,
+      editor: this.renderingEditor?.documentEditor,
+    });
+
+    this.renderingEditor?.documentEditor.open(text);
+  };
+}
+
 export const BmpGenerator = observer<{
   className?: string;
   serviceUrl: string;
   replacements?: ReplacementProps;
   onEditor?(editor: DocumentEditorContainer): void;
-  RenderedDocumentContainer(props: { children: React.ReactNode }): JSX.Element;
-  showDocumentRender: boolean;
+  onInit?(state: BmpGeneratorState): void;
+  renderEditorRef?: React.MutableRefObject<null | DocumentEditorContainer>;
+  editorProps?: Partial<PropsOf<typeof DocumentEditorContainerComponent>>;
 }>(
   ({
     serviceUrl,
     replacements = {},
     onEditor,
     className,
-    RenderedDocumentContainer,
-    showDocumentRender,
+    renderEditorRef,
+    editorProps,
+    onInit,
   }) => {
     const editorRef = React.useRef<null | DocumentEditorContainer>(null);
-    const renderedEditorRef = React.useRef<null | DocumentEditorContainer>(
-      null,
-    );
     const [state] = React.useState(
       () =>
-        new (class State {
-          constructor() {
-            makeAutoObservable(this);
-
-            document.addEventListener('resize', () => {
-              this.editor?.refresh();
-            });
-          }
-
-          documentIsReady = new BooleanModel(false);
-          documentLastChanged = new StateModel(Date.now());
-          documentEditor = new StateModel<DocumentEditorContainer | undefined>(
-            undefined,
-          );
-
-          renderedDocumentEditor = new StateModel<
-            DocumentEditorContainer | undefined
-          >(undefined);
-
-          isPreviewOpen = new BooleanModel(false);
-
-          replacements = new StateModel<ReplacementProps | undefined>(
-            undefined,
-          );
-
-          get editor() {
-            return this.documentEditor.value;
-          }
-
-          get renderingEditor() {
-            return this.renderedDocumentEditor.value;
-          }
-
-          initEditor = (editor?: DocumentEditorContainer) => {
-            if (!editor) return;
-
-            editor.resize();
-          };
-
-          save = () => {
-            const data = this.editor?.documentEditor.serialize();
-
-            console.log({ data });
-
-            this.editor?.documentEditor.save('file.docx', 'Docx');
-          };
-
-          loadDocumentRender = async () => {
-            const blob = await this.editor!.documentEditor.saveAsBlob('Docx')!;
-            const file = new FileReader();
-
-            file.addEventListener('loadend', async (e) => {
-              const base64String = file.result;
-
-              const res = await fetch(
-                `${serviceUrl}/PreviewParameterReplacement`,
-                {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json;charset=UTF-8',
-                  },
-                  body: JSON.stringify({
-                    fileName: this.editor?.documentEditor.documentName,
-                    documentContent: base64String,
-                    textReplacements: [
-                      ...(this.replacements.value?.textReplacements ?? []),
-                    ],
-                    imageReplacements: [
-                      ...(this.replacements.value?.imageReplacements ?? []),
-                    ],
-                    tableReplacements: [
-                      ...(this.replacements.value?.tableReplacements ?? []),
-                    ],
-                  } as PreviewBody),
-                },
-              );
-
-              const text = await res.text();
-
-              this.loadRender(text);
-            });
-
-            file.readAsDataURL(blob);
-          };
-
-          loadRender = (text: string) => {
-            console.log({
-              documentText: text,
-              editor: this.renderingEditor?.documentEditor,
-            });
-
-            this.renderingEditor?.documentEditor.open(text);
-          };
-        })(),
+        new BmpGeneratorState({
+          serviceUrl,
+        }),
     );
 
     React.useEffect(() => {
+      onInit?.(state);
+    }, []);
+
+    React.useEffect(() => {
+      state.serviceUrl.set(serviceUrl);
+    }, [serviceUrl]);
+
+    React.useEffect(() => {
       if (editorRef.current) {
+        console.log({
+          'editorRef.current': editorRef?.current,
+        });
+
         state.documentEditor.set(editorRef.current);
         state.initEditor(state.documentEditor.value);
         onEditor?.(state.documentEditor.value!);
@@ -166,84 +180,99 @@ export const BmpGenerator = observer<{
     }, [editorRef.current]);
 
     React.useEffect(() => {
-      if (renderedEditorRef.current) {
+      if (renderEditorRef?.current) {
         console.log({
-          'renderedEditorRef.current': renderedEditorRef.current,
+          'renderedEditorRef.current': renderEditorRef?.current,
         });
 
-        state.renderedDocumentEditor.set(renderedEditorRef.current);
+        state.renderedDocumentEditor.set(renderEditorRef?.current);
         state.initEditor(state.renderedDocumentEditor.value);
       }
-    }, [renderedEditorRef.current]);
+    }, [renderEditorRef?.current]);
 
     React.useEffect(() => {
       state.replacements.set(replacements);
     }, [replacements]);
 
-    React.useEffect(() => {
-      if (showDocumentRender) {
-        state.loadDocumentRender();
-      }
-    }, [showDocumentRender]);
-
     return (
       <>
         <$EditorWrapper {...{ className }}>
-          <DocumentEditorContainerComponent
-            ref={editorRef as any}
-            documentChange={(e) => {
-              state.documentLastChanged.set(Date.now());
-            }}
-            height={'100%'}
-            serviceUrl={serviceUrl}
-            enableToolbar
-            showPropertiesPane={false}
-            enableComment={false}
-            toolbarItems={[
-              'New',
-              'Open',
-              'Separator',
-              'Undo',
-              'Redo',
-              'Separator',
-              'Image',
-              'Table',
-              'Hyperlink',
-              'Bookmark',
-              'TableOfContents',
-              'Separator',
-              'Header',
-              'Footer',
-              'Separator',
-              'PageSetup',
-              'PageNumber',
-              'Break',
-              'Separator',
-              'InsertFootnote',
-              'InsertEndnote',
-              'Separator',
-              'Find',
-            ]}
-          />
+          <AutoSizer defaultHeight={800} defaultWidth={600}>
+            {({ height, width }) => (
+              <DocumentEditorContainerComponent
+                key={serviceUrl}
+                ref={editorRef as any}
+                documentChange={(e) => {
+                  state.documentLastChanged.set(Date.now());
+                }}
+                height={height + 'px'}
+                width={width + 'px'}
+                serviceUrl={serviceUrl}
+                enableToolbar
+                showPropertiesPane={false}
+                enableComment={false}
+                toolbarItems={[
+                  'New',
+                  'Open',
+                  'Separator',
+                  'Undo',
+                  'Redo',
+                  'Separator',
+                  'Image',
+                  'Table',
+                  'Hyperlink',
+                  'Bookmark',
+                  'TableOfContents',
+                  'Separator',
+                  'Header',
+                  'Footer',
+                  'Separator',
+                  'PageSetup',
+                  'PageNumber',
+                  'Break',
+                  'Separator',
+                  'InsertFootnote',
+                  'InsertEndnote',
+                  'Separator',
+                  'Find',
+                ]}
+                {...editorProps}
+              />
+            )}
+          </AutoSizer>
         </$EditorWrapper>
-        <RenderedDocumentContainer>
-          {/* this is preventing the renderedEditorRef from working. Need to mount it with a portal or something?? */}
-          <DocumentEditorContainerComponent
-            ref={renderedEditorRef as any}
-            height={'100%'}
-            serviceUrl={serviceUrl}
-            restrictEditing
-            showPropertiesPane={false}
-            enableToolbar={false}
-            enableComment={false}
-          />
-        </RenderedDocumentContainer>
       </>
     );
   },
 );
 
+export const BmpGeneratorRenderEditor = observer<{
+  editorRef: React.Ref<any>;
+  serviceUrl: string;
+  editorProps?: Partial<PropsOf<typeof DocumentEditorContainerComponent>>;
+}>(({ editorRef, serviceUrl, editorProps }) => {
+  return (
+    <AutoSizer defaultHeight={800} defaultWidth={600}>
+      {({ height, width }) => (
+        <DocumentEditorContainerComponent
+          key={serviceUrl}
+          ref={editorRef as any}
+          height={height + 'px'}
+          width={width + 'px'}
+          serviceUrl={serviceUrl}
+          restrictEditing
+          showPropertiesPane={false}
+          enableToolbar={false}
+          enableComment={false}
+          {...editorProps}
+        />
+      )}
+    </AutoSizer>
+  );
+});
+
 const $EditorWrapper = styled.div`
   width: 100%;
   height: 100%;
+  overflow: hidden;
 `;
